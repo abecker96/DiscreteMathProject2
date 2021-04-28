c4f8788d5a78b8e83b670287720c96d0fb9238ce
Aidan Becker <49326603+abecker96@users.noreply.github.com>
2021-04-28 09:20:01 -0500

    Completed Part 2
    In addition to the plot, more precise data is output to the console

--- DM_Project2_Part2.py
+++ DM_Project2_Part2.py
@@ -1,17 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 24 14:29:50 2020
+Created on Wed Feb 28 2021
 
-@author: kel
+@author: Aidan Becker and Abrar Ahmad
 """
-print('#####################################################################')
-print('####### Directed weighted network: Florida Ecosystem wet ############')
-print('#####################################################################') 
-#This script shows how to import a dataset into Python that is downloaded from KONECT.
 
-import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
+from timeit import default_timer as timer
+
+# Define insertionSort algorithm
+def insertionSort(arr):
+    # Algorithm listed in textbook:
+    # x = a[k]
+    # j = k-1
+    # while(j != 0)
+    #   if(x<a[j])
+    #       a[j+1] = a[j]
+    #       a[j] = x
+    #       j = j-1
+    # next k
+    
+    # loop through every element of the array
+    for k in range(1, len(arr)):
+        # set x to be the element we're looking at (key element)
+        x = arr[k]
+        # set j to be the index of the element before this one in the array (inspection element)
+        j = k-1
+        # while there still is an element before the one we're looking at
+        while(j >= 0):
+            # if the inspection element before the key element is larger
+            if(x < arr[j]):
+                # swap the positions of the key and inspection elements
+                arr[j+1] = arr[j]
+                arr[j] = x
+            # decrement index of inspection element
+            j = j-1
+   
+# Define a helper function to reverse an array
+def Reverse(lst):
+    return [ele for ele in reversed(lst)]
 
 ################### START: Clearing charts from memory ######################
 
@@ -22,17 +49,6 @@ plt.close('all')
 
 ################### END: Clearing charts from memory ######################
 
-##################### START: Example network graphing #######################
-
-# next few lines plot the layout of the network
-#plt.figure(101, figsize=(10, 10))
-#plt.axes([0,0,0.7,0.6])
-#plt.title('Directed Network: Florida Ecosystem, Wet')
-#pos = nx.spring_layout(dg) # spring_layout is an layout of the vertices.
-#nx.draw_networkx_nodes(dg, pos, node_size = 25) # this plots nodes
-#nx.draw_networkx_edges(dg, pos, width = .5) # this plot edges
-
-##################### END: Example graphing from previous project #######################
 
 ##################### START: Example Histogram #####################
 
@@ -42,3 +58,42 @@ plt.close('all')
 # plt.hist(vec_deg, bins = 10) 
 
 ##################### END: Example Histogram #######################
+
+
+##################### START: Generate arrays to sort #####################
+
+numElements = [1]   #initialized with 1 to make generation easier
+arrN = [[1], [], [], [], [], [], [], [], [], [], []]    #also initialized with 1
+
+for i in range(1, 11):
+    startVal = (i-1)*100 + 100
+    for j in range(1, startVal + 1):
+        arrN[i].append(j)
+    numElements.append(startVal)
+    # need to reverse the list now, it was added as a sorted list
+    arrN[i] = Reverse(arrN[i])
+    
+##################### END: Generate arrays to sort #####################
+
+##################### START: Sort the arrays #####################
+
+timeTakenArr = []   #empty array to store timing information
+
+for arr in arrN:
+    startTime = timer()
+    
+    insertionSort(arr)
+    
+    endTime = timer()
+    print("Time taken to sort an array of " + str(len(arr)) + " elements in milliseconds: " + str((endTime - startTime) * 1000))
+    timeTakenArr.append((endTime - startTime) * 1000)
+
+# plot the data
+plt.scatter(numElements, timeTakenArr)
+plt.xlabel("Number of elements in the array")
+plt.ylabel("Time taken to sort (milliseconds)")
+plt.title("Number of elements vs time taken to sort with insertion sort")
+plt.show()
+
+
+##################### END: Sort the arrays #####################
